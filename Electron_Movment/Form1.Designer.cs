a55633cffec211be0defd0e48796f69cb06a4db4
Ebtekar.Ali <Ebtekar.Ali@C19-01>
2024-11-28 20:08:21 +0100

    Add project files.

---
+++ Electron_Movment/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Electron_Movment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Battery = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.line1 = new System.Windows.Forms.PictureBox();
+            this.line2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lamp = new System.Windows.Forms.PictureBox();
+            this.line3 = new System.Windows.Forms.PictureBox();
+            this.line4 = new System.Windows.Forms.PictureBox();
+            this.ButtonIncrease = new System.Windows.Forms.Button();
+            this.ButtonDecrease = new System.Windows.Forms.Button();
+            this.Buttonchangelamp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.electron = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Battery)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lamp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electron)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Battery
+            // 
+            this.Battery.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Battery.Location = new System.Drawing.Point(638, 362);
+            this.Battery.Name = "Battery";
+            this.Battery.Size = new System.Drawing.Size(127, 60);
+            this.Battery.TabIndex = 0;
+            this.Battery.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(655, 341);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(716, 341);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 31);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // line1
+            // 
+            this.line1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.line1.Location = new System.Drawing.Point(726, 62);
+            this.line1.Name = "line1";
+            this.line1.Size = new System.Drawing.Size(10, 310);
+            this.line1.TabIndex = 3;
+            this.line1.TabStop = false;
+            // 
+            // line2
+            // 
+            this.line2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.line2.Location = new System.Drawing.Point(128, 62);
+            this.line2.Name = "line2";
+            this.line2.Size = new System.Drawing.Size(608, 10);
+            this.line2.TabIndex = 4;
+            this.line2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pictureBox4.Location = new System.Drawing.Point(324, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(127, 108);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lamp
+            // 
+            this.lamp.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lamp.Location = new System.Drawing.Point(92, 36);
+            this.lamp.Name = "lamp";
+            this.lamp.Size = new System.Drawing.Size(82, 60);
+            this.lamp.TabIndex = 6;
+            this.lamp.TabStop = false;
+            // 
+            // line3
+            // 
+            this.line3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.line3.Location = new System.Drawing.Point(126, 62);
+            this.line3.Name = "line3";
+            this.line3.Size = new System.Drawing.Size(10, 310);
+            this.line3.TabIndex = 7;
+            this.line3.TabStop = false;
+            // 
+            // line4
+            // 
+            this.line4.BackColor = System.Drawing.Color.Red;
+            this.line4.Location = new System.Drawing.Point(126, 362);
+            this.line4.Name = "line4";
+            this.line4.Size = new System.Drawing.Size(609, 10);
+            this.line4.TabIndex = 8;
+            this.line4.TabStop = false;
+            // 
+            // ButtonIncrease
+            // 
+            this.ButtonIncrease.Location = new System.Drawing.Point(354, 12);
+            this.ButtonIncrease.Name = "ButtonIncrease";
+            this.ButtonIncrease.Size = new System.Drawing.Size(77, 37);
+            this.ButtonIncrease.TabIndex = 9;
+            this.ButtonIncrease.Text = "Plus";
+            this.ButtonIncrease.UseVisualStyleBackColor = true;
+            // 
+            // ButtonDecrease
+            // 
+            this.ButtonDecrease.Location = new System.Drawing.Point(354, 78);
+            this.ButtonDecrease.Name = "ButtonDecrease";
+            this.ButtonDecrease.Size = new System.Drawing.Size(75, 37);
+            this.ButtonDecrease.TabIndex = 10;
+            this.ButtonDecrease.Text = "Minus";
+            this.ButtonDecrease.UseVisualStyleBackColor = true;
+            // 
+            // Buttonchangelamp
+            // 
+            this.Buttonchangelamp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Buttonchangelamp.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Buttonchangelamp.Location = new System.Drawing.Point(305, 184);
+            this.Buttonchangelamp.Name = "Buttonchangelamp";
+            this.Buttonchangelamp.Size = new System.Drawing.Size(160, 80);
+            this.Buttonchangelamp.TabIndex = 11;
+            this.Buttonchangelamp.Text = "Lamp Color";
+            this.Buttonchangelamp.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Electron Pass";
+            // 
+            // electron
+            // 
+            this.electron.BackColor = System.Drawing.Color.Yellow;
+            this.electron.Location = new System.Drawing.Point(716, 275);
+            this.electron.Name = "electron";
+            this.electron.Size = new System.Drawing.Size(34, 34);
+            this.electron.TabIndex = 13;
+            this.electron.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Battery);
+            this.Controls.Add(this.ButtonDecrease);
+            this.Controls.Add(this.ButtonIncrease);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.lamp);
+            this.Controls.Add(this.electron);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Buttonchangelamp);
+            this.Controls.Add(this.line2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.line4);
+            this.Controls.Add(this.line3);
+            this.Controls.Add(this.line1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Battery)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lamp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electron)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Battery;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox line1;
+        private System.Windows.Forms.PictureBox line2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox lamp;
+        private System.Windows.Forms.PictureBox line3;
+        private System.Windows.Forms.PictureBox line4;
+        private System.Windows.Forms.Button ButtonIncrease;
+        private System.Windows.Forms.Button ButtonDecrease;
+        private System.Windows.Forms.Button Buttonchangelamp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox electron;
+    }
+}
+
---
+++ Electron_Movment/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Electron_Movment
+{
+    public partial class Form1 : Form
+    {
+        Timer ElectronTimer = new Timer();
+        int voltspeed = 12;
+        int dirextionX = 0;//-1 = left, 1=right, 0 = vertical
+        int directionY = -1; // -1 = up, 1= down, 0 = horizantal
+        int electronResult = 0;
+        Timer GenerateTimer = new Timer();
+        int RandomSpeed = 300000;
+        int Distance = 0;
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        void Start()
+        {
+            Setuptimer();
+            Addevent();
+            label1.Font = new Font(label1.Font.FontFamily, 10, FontStyle.Bold);
+            CalculateDistance();
+
+
+        }
+
+        void CalculateDistance()
+        {
+
+            Distance = (line1.Top - line3.Bottom) + (line2.Left - line4.Right) + (line3.Bottom - line1.Top) + (line4.Right - line2.Left);
+        }
+
+        void Setuptimer()
+        {
+            ElectronTimer.Interval = 16;
+            ElectronTimer.Start();
+            ElectronTimer.Tick += ElectronEvent;
+
+            GenerateTimer.Interval = RandomSpeed;
+            GenerateTimer.Start();
+            GenerateTimer.Tick += GenerateEvent;
+        }
+
+        void GenerateEvent(object s, EventArgs e)
+        {
+            PictureBox Electron = new PictureBox();
+            Electron.Width = 30;
+            Electron.Height = 30;
+
+            Electron.BackColor = Color.Blue;
+
+
+            Electron.Location = new Point(line1.Left - line1.Width / 2);
+            Electron.Top = 150;
+
+
+            this.Controls.Add(Electron);
+
+            voltspeed = Distance / RandomSpeed / ElectronTimer.Interval;
+            electron.Left = (line1.Left + line1.Right) / 2;
+            electron.Top = line1.Top;
+            dirextionX = -1;
+            directionY = 0;
+
+            electronResult = 0;
+            label1.Text = $" Electron Passes {electronResult}";
+
+
+        }
+
+
+
+        void Addevent()
+        {
+            ButtonIncrease.Click += Uppervoltage;
+            ButtonDecrease.Click += Lowervoltage;
+            Buttonchangelamp.Click += LampChange;
+        }
+
+        void LampChange(object s, EventArgs e)
+        {
+            lamp.BackColor = Color.ForestGreen;
+        }
+
+        void Uppervoltage(object s, EventArgs e)
+        {
+            if (voltspeed < 24)
+            {
+                voltspeed += 1;
+                label1.Text = voltspeed.ToString();
+                label1.Text = $"Volt speed {voltspeed}";
+
+            }
+        }
+
+        void Lowervoltage(object s, EventArgs e)
+        {
+            if (voltspeed > 1)
+            {
+                voltspeed -= 1;
+                label1.Text = voltspeed.ToString();
+                label1.Text = $"Volt speed {voltspeed}";
+            }
+        }
+
+        void ElectronEvent(object s, EventArgs e)
+        {
+            if (dirextionX == 0 && directionY == -1)
+            {
+                if (electron.Top > line1.Top)
+                {
+                    electron.Top -= voltspeed;
+                }
+                else
+                {
+                    electron.Top = line1.Top;
+                    dirextionX = -1;
+                    directionY = 0;
+                }
+            }
+
+            else if (dirextionX == -1 && directionY == 0)
+            {
+                if (electron.Left > line2.Left)
+                {
+                    electron.Left -= voltspeed;
+                }
+                else
+                {
+                    electron.Left = line2.Left;
+                    dirextionX = 0;
+                    directionY = 1;
+                }
+            }
+            else if (dirextionX == 0 && directionY == 1)
+            {
+                if (electron.Top < line3.Bottom - electron.Height + 10)
+                {
+                    electron.Top += voltspeed;
+                }
+                else
+                {
+                    electron.Top = line3.Bottom - electron.Height + 10;
+                    dirextionX = 1;
+                    directionY = 0;
+                }
+            }
+            else if (dirextionX == 1 && directionY == 0)
+            {
+                if (electron.Left < line4.Right - electron.Width + 10)
+                {
+                    electron.Left += voltspeed;
+
+                }
+
+                else
+                {
+                    electron.Left = line4.Right - electron.Width + 10;
+                    dirextionX = 0;
+                    directionY = -1;
+                    electronResult += 1;
+                    label1.Text = $"Electron Passes {electronResult}";
+                    electron.Visible = true;
+                }
+            }
+
+            if (electron.Left > lamp.Left && electron.Left < lamp.Right && electron.Top > lamp.Top && electron.Top < lamp.Bottom)
+            {
+                lamp.BackColor = Color.Gold;
+            }
+        }
+    }
+}
